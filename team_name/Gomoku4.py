8654af1ad56d5d703efeb45678b4bf1e4e68282d
Dragon11150221 <jinglong@ualberta.ca>
2021-12-13 21:17:19 -0700

    upload

---
+++ team_name/Gomoku4.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+#/usr/local/bin/python3
+# Set the path to your python3 above
+
+from gtp_connection import GtpConnection
+from board_util import GoBoardUtil, EMPTY
+from simple_board import SimpleGoBoard
+
+import random
+import numpy as np
+
+def undo(board,move):
+    board.board[move]=EMPTY
+    board.current_player=GoBoardUtil.opponent(board.current_player)
+
+def play_move(board, move, color):
+    board.play_move_gomoku(move, color)
+
+def game_result(board):
+    game_end, winner = board.check_game_end_gomoku()
+    moves = board.get_empty_points()
+    board_full = (len(moves) == 0)
+    if game_end:
+        #return 1 if winner == board.current_player else -1
+        return winner
+    if board_full:
+        return 'draw'
+    return None
+
+class GomokuSimulationPlayer(object):
+    """
+    For each move do `n_simualtions_per_move` playouts,
+    then select the one with best win-rate.
+    playout could be either random or rule_based (i.e., uses pre-defined patterns)
+    """
+    def __init__(self, n_simualtions_per_move=10, playout_policy='random', board_size=7):
+        assert(playout_policy in ['random', 'rule_based'])
+        self.n_simualtions_per_move=n_simualtions_per_move
+        self.board_size=board_size
+        self.playout_policy=playout_policy
+
+        #NOTE: pattern has preference, later pattern is ignored if an earlier pattern is found
+        self.pattern_list=['Win', 'BlockWin', 'OpenFour', 'BlockOpenFour', 'Random']
+
+        self.name="team_name_engine"
+        self.version = 9.9
+        self.best_move=None
+
+    def set_playout_policy(self, playout_policy='random'):
+        assert(playout_policy in ['random', 'rule_based'])
+        self.playout_policy=playout_policy
+
+    def _random_moves(self, board, color_to_play):
+        return GoBoardUtil.generate_legal_moves_gomoku(board)
+
+    def policy_moves(self, board, color_to_play):
+
+        assert(isinstance(board, SimpleGoBoard))
+        ret=board.get_pattern_moves()
+        movetype_id, moves=ret
+        return self.pattern_list[movetype_id], moves
+
+    def _do_playout(self, board, color_to_play):
+        res=game_result(board)
+        simulation_moves=[]
+        while(res is None):
+            _ , candidate_moves = self.policy_moves(board, board.current_player)
+            playout_move=random.choice(candidate_moves)
+            play_move(board, playout_move, board.current_player)
+            simulation_moves.append(playout_move)
+            res=game_result(board)
+        for m in simulation_moves[::-1]:
+            undo(board, m)
+        if res == color_to_play:
+            return 1.0
+        elif res == 'draw':
+            return 0.0
+        else:
+            assert(res == GoBoardUtil.opponent(color_to_play))
+            return -1.0
+
+    def get_move(self, board, color_to_play):
+        """
+        The genmove function called by gtp_connection
+        """
+        moves=GoBoardUtil.generate_legal_moves_gomoku(board)
+        moves = self.sort_value_moves(moves)
+        toplay=board.current_player
+        best_result, best_move=-1.1, None
+        best_move=moves[0]
+        wins = np.zeros(len(moves))
+        visits = np.zeros(len(moves))
+        while True:
+            for i, move in enumerate(moves):
+                play_move(board, move, toplay)
+                res=game_result(board)
+                if res == toplay:
+                    undo(board, move)
+                    #This move is a immediate win
+                    self.best_move=move
+                    return move
+                ret=self._do_playout(board, toplay)
+                wins[i] += ret
+                visits[i] += 1
+                win_rate = wins[i] / visits[i]
+                if win_rate > best_result:
+                    best_result=win_rate
+                    best_move=move
+                    self.best_move=best_move
+                undo(board, move)
+        assert(best_move is not None)
+        return best_move
+
+    def point_weight(self, point):
+        color = self.current_player
+        check_list = [-1, 1, -self.NS, self.NS, -self.NS-1, - self.NS + 1, + self.NS - 1, + self.NS + 1]
+        weight = 0
+        for direction in check_list:
+            a = 1
+            b = 1
+            temp_weight = 0
+            for i in range(4):
+                next = point + direction*(i+1)
+                if next >= len(self.board):
+                    break
+                if self.get_color(next) == color:
+                    temp_weight += a
+                    a *= 3
+                    if i == b-1:
+                        b += 1
+            temp_weight *= b
+            weight += temp_weight
+        return weight
+
+    def sort_value_moves(self,value_moves):
+        move_weight = dict()
+        for move in value_moves:
+            weight = self.point_weight(move)
+            move_weight[move] = weight
+
+
+        sorted_moves = sorted(move_weight.keys(), key=lambda d: move_weight[d], reverse = True)
+        return sorted_moves
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def run():
+    """
+    start the gtp connection and wait for commands.
+    """
+    board = SimpleGoBoard(7)
+    con = GtpConnection(GomokuSimulationPlayer(), board)
+    con.start_connection()
+
+if __name__=='__main__':
+    run()
