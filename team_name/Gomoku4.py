2df65d0465f340b048efdd29beef65520315d6e2
Dragon11150221 <jinglong@ualberta.ca>
2021-12-16 14:28:21 -0700

    MCTS

--- team_name/Gomoku4.py
+++ team_name/Gomoku4.py
@@ -5,6 +5,7 @@
 from gtp_connection import GtpConnection
 from board_util import GoBoardUtil, EMPTY
 from simple_board import SimpleGoBoard
+from mcts import MCTS
 
 import random
 import numpy as np
@@ -33,102 +34,118 @@ class GomokuSimulationPlayer(object):
     then select the one with best win-rate.
     playout could be either random or rule_based (i.e., uses pre-defined patterns)
     """
-    def __init__(self, n_simualtions_per_move=10, playout_policy='random', board_size=7):
-        assert(playout_policy in ['random', 'rule_based'])
-        self.n_simualtions_per_move=n_simualtions_per_move
-        self.board_size=board_size
-        self.playout_policy=playout_policy
-
-        #NOTE: pattern has preference, later pattern is ignored if an earlier pattern is found
-        self.pattern_list=['Win', 'BlockWin', 'OpenFour', 'BlockOpenFour', 'Random']
-
-        self.name="team_name_engine"
-        self.version = 9.9
-        self.best_move=None
-
-    def set_playout_policy(self, playout_policy='random'):
-        assert(playout_policy in ['random', 'rule_based'])
-        self.playout_policy=playout_policy
-
-    def _random_moves(self, board, color_to_play):
-        return GoBoardUtil.generate_legal_moves_gomoku(board)
-
-    def policy_moves(self, board, color_to_play):
-
-        assert(isinstance(board, SimpleGoBoard))
-        ret=board.get_pattern_moves()
-        movetype_id, moves=ret
-        return self.pattern_list[movetype_id], moves
-
-    def _do_playout(self, board, color_to_play):
-        res=game_result(board)
-        simulation_moves=[]
-        while(res is None):
-            _ , candidate_moves = self.policy_moves(board, board.current_player)
-            playout_move=random.choice(candidate_moves)
-            play_move(board, playout_move, board.current_player)
-            simulation_moves.append(playout_move)
-            res=game_result(board)
-        for m in simulation_moves[::-1]:
-            undo(board, m)
-        if res == color_to_play:
-            return 1.0
-        elif res == 'draw':
-            return 0.0
-        else:
-            assert(res == GoBoardUtil.opponent(color_to_play))
-            return -1.0
-
-    def get_move(self, board, color_to_play):
+    def __init__(
+        self,
+        num_sim,
+        sim_rule,
+        move_filter,
+        in_tree_knowledge,
+        size=7,
+        limit=100,
+        exploration=0.4,
+    ):
         """
-        The genmove function called by gtp_connection
+        Player that selects a move based on MCTS from the set of legal moves
         """
-        moves=GoBoardUtil.generate_legal_moves_gomoku(board)
-        toplay=board.current_player
-        moves = self.sort_value_moves(moves,toplay, board)
-        print(moves)
-        self.best_move = moves[0]
-
-        POSSIBLE_TYPES = ['Win', 'BlockWin', 'OpenFour', 'BlockOpenFour', 'Random']
-        for type in POSSIBLE_TYPES:
-            action_list = board.policy(type)
-            if len(action_list) != 0:
-                best_move = action_list[0]
-                self.best_move = action_list[0]
-
-
-
-        return best_move
-
-    def point_weight(self, point, color, board):
-        check_list = [-1, 1, -board.NS, board.NS, -board.NS-1, - board.NS + 1, + board.NS - 1, + board.NS + 1]
-        weight = 0
-        for direction in check_list:
-            a = 1
-            b = 1
-            temp_weight = 0
-            for i in range(4):
-                next = point + direction*(i+1)
-                if next >= len(board.board):
-                    break
-                if board.get_color(next) == color:
-                    temp_weight += a
-                    a *= 3
-                    if i == b-1:
-                        b += 1
-            temp_weight *= b
-            weight += temp_weight
-        return weight
-
-    def sort_value_moves(self,value_moves,toplay, board):
-        move_weight = dict()
-        for move in value_moves:
-            weight = self.point_weight(move,toplay, board)
-            move_weight[move] = weight
+        self.name = "Gomoku4"
+        self.version = "1.0"
+        self.komi = 6.5
+        self.MCTS = MCTS()
+        self.num_simulation = num_sim
+        self.limit = limit
+        self.exploration = exploration
+        self.simulation_policy = sim_rule
+        self.use_pattern = True
+        self.check_selfatari = move_filter
+        self.in_tree_knowledge = in_tree_knowledge
+        self.parent = None
 
 
-        sorted_moves = sorted(move_weight.keys(), key=lambda d: move_weight[d], reverse = True)
-        return sorted_moves
+    def reset(self):
+        self.MCTS = MCTS()
+
+    def update(self, move):
+        self.parent = self.MCTS._root
+        self.MCTS.update_with_move(move)
+
+    # def set_playout_policy(self, playout_policy='random'):
+    #     assert(playout_policy in ['random', 'rule_based'])
+    #     self.playout_policy=playout_policy
+
+    # def _random_moves(self, board, color_to_play):
+    #     return GoBoardUtil.generate_legal_moves_gomoku(board)
+
+    # def policy_moves(self, board, color_to_play):
+
+    #     assert(isinstance(board, SimpleGoBoard))
+    #     ret=board.get_pattern_moves()
+    #     movetype_id, moves=ret
+    #     return self.pattern_list[movetype_id], moves
+
+    # def _do_playout(self, board, color_to_play):
+    #     res=game_result(board)
+    #     simulation_moves=[]
+    #     while(res is None):
+    #         _ , candidate_moves = self.policy_moves(board, board.current_player)
+    #         playout_move=random.choice(candidate_moves)
+    #         play_move(board, playout_move, board.current_player)
+    #         simulation_moves.append(playout_move)
+    #         res=game_result(board)
+    #     for m in simulation_moves[::-1]:
+    #         undo(board, m)
+    #     if res == color_to_play:
+    #         return 1.0
+    #     elif res == 'draw':
+    #         return 0.0
+    #     else:
+    #         assert(res == GoBoardUtil.opponent(color_to_play))
+    #         return -1.0
+
+    def get_move(self, board, toplay):
+        move = self.MCTS.get_move(
+            board,
+            toplay,
+            komi=self.komi,
+            limit=self.limit,
+            check_selfatari=self.check_selfatari,
+            use_pattern=self.use_pattern,
+            num_simulation=self.num_simulation,
+            exploration=self.exploration,
+            simulation_policy=self.simulation_policy,
+            in_tree_knowledge=self.in_tree_knowledge,
+        )
+        self.update(move)
+        return move
+
+    # def point_weight(self, point, color, board):
+    #     check_list = [-1, 1, -board.NS, board.NS, -board.NS-1, - board.NS + 1, + board.NS - 1, + board.NS + 1]
+    #     weight = 0
+    #     for direction in check_list:
+    #         a = 1
+    #         b = 1
+    #         temp_weight = 0
+    #         for i in range(4):
+    #             next = point + direction*(i+1)
+    #             if next >= len(board.board):
+    #                 break
+    #             if board.get_color(next) == color:
+    #                 temp_weight += a
+    #                 a *= 3
+    #                 if i == b-1:
+    #                     b += 1
+    #         temp_weight *= b
+    #         weight += temp_weight
+    #     return weight
+
+    # def sort_value_moves(self,value_moves,toplay, board):
+    #     move_weight = dict()
+    #     for move in value_moves:
+    #         weight = self.point_weight(move,toplay, board)
+    #         move_weight[move] = weight
+
+
+    #     sorted_moves = sorted(move_weight.keys(), key=lambda d: move_weight[d], reverse = True)
+    #     return sorted_moves
 
 
 
